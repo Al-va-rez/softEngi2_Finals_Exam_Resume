d1c918df5a7234f60481f35a124777872b5c4501
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2025-12-10 15:51:46 +0800

    Add files via upload

---
+++ exam/dynamic/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2" defer></script>
+
+    <!-- Bootstrap 4 JS + dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
+    <?php include 'aboutMe.php'; ?>
+    <?php include 'projects.php'; ?>
+    <?php include 'education_certificates.php'; ?>
+    <?php include 'techSkills.php'; ?>
+</body>
+</html>
